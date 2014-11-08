41432d6c0300733581c86d34cac3deb4fc4261f1
cjpwnzyou <chris.johnson2011@hotmail.com>
2014-11-08 17:42:25 +1100

    Update Program.cs
    Added Jinx, Azir and Vel'Koz

--- VoliBotARAM/Program.cs
+++ VoliBotARAM/Program.cs
@@ -16,8 +16,8 @@ namespace VoliBotARAM
     {
         static string[] assasins = new string[] { "Akali", "Diana", "Evelynn", "Fizz", "Katarina", "Nidalee" };
         static string[] adtanks = new string[] { "Braum", "DrMundo", "Garen", "Hecarim", "Jarvan IV", "Nasus", "Skarner", "Volibear", "Yorick", "Gnar" };
-        static string[] adcs = new string[] { "Ashe", "Caitlyn", "Corki", "Draven", "Ezreal", "Gankplank", "Graves", "KogMaw", "Lucian", "MissFortune", "Quinn", "Sivir", "Thresh", "Tristana", "Tryndamere", "Twitch", "Urgot", "Varus", "Vayne", "Kalista" };
-        static string[] mages = new string[] { "Ahri", "Anivia", "Annie", "Brand", "Cassiopeia", "Galio", "Gragas", "Heimerdinger", "Janna", "Karma", "Karthus", "LeBlanc", "Lissandra", "Lulu", "Lux", "Malzahar", "Morgana", "Nami", "Nunu", "Orianna", "Ryze", "Sona", "Soraka", "Swain", "Syndra", "Taric", "TwistedFate", "Veigar", "Viktor", "Xerath", "Ziggs", "Zilian", "Zyra" };
+        static string[] adcs = new string[] { "Ashe", "Caitlyn", "Corki", "Draven", "Ezreal", "Gankplank", "Graves","Jinx", "KogMaw", "Lucian", "MissFortune", "Quinn", "Sivir", "Thresh", "Tristana", "Tryndamere", "Twitch", "Urgot", "Varus", "Vayne", "Kalista" };
+        static string[] mages = new string[] { "Ahri", "Anivia", "Annie","Azir", "Brand", "Cassiopeia", "Galio", "Gragas", "Heimerdinger", "Janna", "Karma", "Karthus", "LeBlanc", "Lissandra", "Lulu", "Lux", "Malzahar", "Morgana", "Nami", "Nunu", "Orianna", "Ryze", "Sona", "Soraka", "Swain", "Syndra", "Taric", "TwistedFate", "Veigar","Vel'Koz", "Viktor", "Xerath", "Ziggs", "Zilian", "Zyra" };
         static string[] hybrids = new string[] { "Kayle", "Teemo" };
         static string[] aptanks = new string[] { "Alistar", "Amumu", "Blitzcrank", "ChoGath", "Leona", "Malphite", "Maokai", "Nautilus", "Rammus", "Sejuani", "Shen", "Singed", "Zac" };
         static string[] bruisers = new string[] { "Darius", "Irelia", "Khazix", "LeeSin", "Olaf", "Pantheon", "Renekton", "Rengar", "Riven", "Shyvana", "Talon", "Trundle", "Vi", "Wukong", "Zed" };
